19453c0fbeb95506b58a3c5596053f12e4b810d7
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-09-28 11:20:39 -0400

    Add sidenav and fontawesome

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { NavbarComponent } from './navbar/navbar.component';
-
-
+import { SidenavModule } from '../shared/directives/sidenav/sidenav.module';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    NavbarComponent
-  ]
+  imports: [CommonModule, SidenavModule],
+  exports: [NavbarComponent]
 })
-export class LayoutModule { }
+export class LayoutModule {}
